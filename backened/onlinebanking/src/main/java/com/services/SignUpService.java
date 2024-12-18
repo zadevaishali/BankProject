147a6871e5edb088e86df987c2e3709c016125a1
Vaishali Zade <110128262+zadevaishali@users.noreply.github.com>
2024-12-18 16:38:33 +0530

    Update SignUpService.java

--- backened/onlinebanking/src/main/java/com/services/SignUpService.java
+++ backened/onlinebanking/src/main/java/com/services/SignUpService.java
@@ -8,7 +8,7 @@ import com.models.User;
 public interface SignUpService {
 	
   public User createUser(User user);
-  public Optional<User> getUser(Long userId);
+  public Optional<User> getUser(String userid);
   public List<User> getAllUsers();
   public void save(User theUser);
   public User findByEmail(String email);
