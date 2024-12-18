384cfbd3c0629932c697fdc97e12bf97869761e4
Vaishali Zade <110128262+zadevaishali@users.noreply.github.com>
2024-12-18 16:34:47 +0530

    Update UserDetailsRepository.java

--- backened/onlinebanking/src/main/java/com/repositories/UserDetailsRepository.java
+++ backened/onlinebanking/src/main/java/com/repositories/UserDetailsRepository.java
@@ -1,9 +1,11 @@
 package com.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import com.models.User;
 import com.models.UserDetail;
 
 @Repository
-public interface UserDetailsRepository extends JpaRepository<UserDetail, Integer> {
-
+public interface UserDetailsRepository extends JpaRepository<UserDetail, Long> {
+	
 }
