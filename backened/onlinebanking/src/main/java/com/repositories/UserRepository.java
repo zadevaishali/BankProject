75137fe8413469a2bdef371474a3f7f29671231d
Vaishali Zade <110128262+zadevaishali@users.noreply.github.com>
2024-12-18 16:34:09 +0530

    Update UserRepository.java

--- backened/onlinebanking/src/main/java/com/repositories/UserRepository.java
+++ backened/onlinebanking/src/main/java/com/repositories/UserRepository.java
@@ -1,11 +1,15 @@
 package com.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.models.User;
 
 @Repository
-public interface UserRepository extends JpaRepository<User,Long> {
+public interface UserRepository extends JpaRepository<User,String> {
 	 public User findByEmail(String email);
+
+	public Optional<User> findById(String userId);
 }
